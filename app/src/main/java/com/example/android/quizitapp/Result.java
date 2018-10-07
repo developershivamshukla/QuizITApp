5472eddb169f0d1667a054d01aed294fac0dc666
Shivam Shukla <developershivamshukla@gmail.com>
2018-10-07 21:36:10 +0530

    Restart and Exit Button Added

--- app/src/main/java/com/example/android/quizitapp/Result.java
+++ app/src/main/java/com/example/android/quizitapp/Result.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Result extends AppCompatActivity {
 
     TextView language, name, correct, wrong, percent, keepPlaying;
+    Button exit,restart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,8 @@ public class Result extends AppCompatActivity {
         wrong = findViewById(R.id.wrong);
         percent = findViewById(R.id.percent);
         keepPlaying = findViewById(R.id.keepPlaying);
+        exit = findViewById(R.id.Exit);
+        restart = findViewById(R.id.Restart);
 
         int acc = QuestionView.acc;
         int wa = QuestionView.wa;
@@ -36,6 +40,20 @@ public class Result extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(Result.this, Home.class);
                 startActivity(i);
+            }
+        });
+
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Result.this,Home.class);
+                startActivity(intent);
+            }
+        });
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
             }
         });
 
